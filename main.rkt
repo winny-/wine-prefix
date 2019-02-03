bf2fd5e4919f5748ceb026e3a81d385993f434c3
Winston Weinert <winston@ml1.net>
2019-02-02 20:24:39 -0600

    source code cleanup

--- main.rkt
+++ main.rkt
@@ -1,6 +1,13 @@
-#lang racket
+#lang racket/base
 
-(require command-tree)
+(require racket/match
+         racket/promise
+         racket/string
+         racket/unit)
+
+; --------------------------------------------------------------------
+; configuration
+; --------------------------------------------------------------------
 
 (struct wine-prefix-settings [profiles] #:prefab)
 (struct wine-prefix-profile [name prefix tasks] #:prefab)
@@ -25,6 +32,37 @@
               #:when (string-ci=? (wine-prefix-profile-name p) name))
     p))
 
+; --------------------------------------------------------------------
+; helpers
+; --------------------------------------------------------------------
+
+(define-syntax with-environment-variables
+  (syntax-rules ()
+    [(_ () body bodies ...)
+     (begin body bodies ...)]
+    [(_ ([a b] pairs ...) body bodies ...)
+     (parameterize ([current-environment-variables (environment-variables-copy (current-environment-variables))])
+       (putenv a b)
+       (with-environment-variables (pairs ...)
+         body bodies ...))]))
+
+(define (simple-subprocess command-path-or-name . args)
+  (define executable-path
+    (if (string-contains? command-path-or-name "/")
+        command-path-or-name
+        (or (find-executable-path command-path-or-name)
+            (error 'simple-subprocess "Could not locate ~a in PATH" command-path-or-name))))
+  (define-values (the-subprocess stdout stdin stderr)
+    (apply subprocess
+           (current-output-port) (current-input-port) (current-error-port)
+           executable-path
+           args))
+  the-subprocess)
+
+; --------------------------------------------------------------------
+; verbs
+; --------------------------------------------------------------------
+
 (define (wine-prefix-run what [task-name "default"])
   (match-define (struct* wine-prefix-profile ([prefix prefix] [tasks tasks])) (wine-prefix-get-profile what))
   (match-define (struct* wine-prefix-task ([name name] [kind kind] [payload payload]))
@@ -32,19 +70,17 @@
                 #:when (string-ci=? (wine-prefix-task-name t) task-name))
       t))
   (with-environment-variables (["WINEPREFIX" prefix])
-    (define-values (wine-subprocess stdout stdin stderr)
-      (apply subprocess (current-output-port) (current-input-port) (current-error-port) (or (find-executable-path "wine") (error 'wine-prefix-run "Could not locate wine in PATH")) payload))
+    (define wine-subprocess (apply simple-subprocess "wine" payload))
     (subprocess-wait wine-subprocess)
     (exit (subprocess-status wine-subprocess))))
 
 (define (wine-prefix-kill what)
   (match-define (struct* wine-prefix-profile ([prefix prefix])) (wine-prefix-get-profile what))
   (with-environment-variables (["WINEPREFIX" prefix])
-    (define-values (wineserver-subprocess stdout stdin stderr)
-      (subprocess (current-output-port) (current-input-port) (current-error-port) (or (find-executable-path "wineserver") (error 'wine-prefix-kill "Could not locate wineserver in PATH"))
-                  "-k"))
+    (define wineserver-subprocess (simple-subprocess "wineserver" "-k"))
     (subprocess-wait wineserver-subprocess)
     (exit (subprocess-status wineserver-subprocess))))
+
 
 (define (wine-prefix-help)
   (printf #<<EOF
@@ -58,16 +94,6 @@ wine-prefix usage:
 EOF
           ))
 
-(define-syntax with-environment-variables
-  (syntax-rules ()
-    [(_ () body bodies ...)
-     (begin body bodies ...)]
-    [(_ ([a b] pairs ...) body bodies ...)
-     (parameterize ([current-environment-variables (environment-variables-copy (current-environment-variables))])
-       (putenv a b)
-       (with-environment-variables (pairs ...)
-         body bodies ...))]))
-
 (define (wine-prefix-list)
   (for ([profile (wine-prefix-settings-profiles (wine-prefix-get-config))])
     (match-define (struct* wine-prefix-profile ([name name] [prefix prefix] [tasks tasks]))
@@ -78,7 +104,12 @@ EOF
         task)
       (printf "\t~a\t~a\t~a\n" task-name kind payload))))
 
+; --------------------------------------------------------------------
+; entrypoint
+; --------------------------------------------------------------------
+
 (module+ main
+  (require command-tree)
   (define wine-prefix-commands
     `([help ,wine-prefix-help]
       [run ,wine-prefix-run]
